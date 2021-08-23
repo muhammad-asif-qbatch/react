8535b2406897ac278d65d22e49acf91fe6fa7bf5
Muhammad Asif <m.asif@qbatch.com>
2021-08-23 13:59:14 +0500

    changing in the classcompnent file

--- src/components/ClassComponent.js
+++ src/components/ClassComponent.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Welcome extends Component {
-    render(){
-        return <h1>Class Components</h1>
+    render() {
+        return <h1>Using the class component</h1>
     }
 }
 export default Welcome;
